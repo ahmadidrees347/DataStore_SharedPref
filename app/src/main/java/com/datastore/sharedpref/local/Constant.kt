499a6a6a11cc3b061168afe533b2ab64d2c45b30
Ahmad Idrees <ahmad.idrees@appinsnap.com>
2023-12-01 10:01:13 +0500

    Initial commit

---
+++ app/src/main/java/com/datastore/sharedpref/local/Constant.kt
@@ -0,0 +1,7 @@
+package com.datastore.sharedpref.local
+
+object Constant {
+    const val IS_PIN_SET = "IS_PIN_SET"
+    const val PIN_CODE = "PIN_CODE"
+    const val STRING_SET_KEY = "STRING_SET_KEY"
+}
